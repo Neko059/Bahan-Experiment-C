e1857466706201f3f0191c85e977094f04362e77
Han <66839648+Neko059@users.noreply.github.com>
2024-01-04 22:42:33 +0700

    C: Add Integer

---
+++ ngga_tau.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+    
+    int luas;
+    int jumlah_bangun;
+
+    int panjang;
+    int lebar;
+
+    printf( "Masukan Panjang: ");
+    scanf( "%d", &panjang ); 
+
+    printf( "Masukan Lebar: ");
+    scanf( "%d", &panjang ); 
+
+    luas= panjang*lebar;
+}
