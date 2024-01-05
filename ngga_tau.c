91d76521e6cb3e7b6988e956f2178163be6f6a6d
Han <66839648+Neko059@users.noreply.github.com>
2024-01-05 10:02:16 +0700

    C: Fix typo doang

--- ngga_tau.c
+++ ngga_tau.c
@@ -2,18 +2,20 @@
 #include <conio.h>
 
 int main (){
-    
+
     int luas,jml_bangun,panjang,lebar,libur,hitung;
     float i;
 
-    printf( "Masukan Lebar: ");
-    scanf( "%d", &jml_bangun ); 
-
+    printf( "Input Jumlah bangun yang akan dihitung Luas-nya! : ");
+    scanf( "%d", &jml_bangun );
+    
     for(i = 10; i <= jml_bangun; i++)
     {
       // Text yang akan muncul di console
-      printf("1. Hitung Luas Persegi\n");
-      printf("2. Hitung Luas Segi Tiga\n");
+      printf("\n\n\n\n");
+      printf("Imput Metode Perhitungan Luas\n");
+      printf("1. Luas Segi tiga\n");
+      printf("2. Luas Persegi\n");
 
       printf("Input Hitung ( 1 atau 2 ): "); 
       scanf("%d", &hitung);
@@ -21,23 +23,30 @@ int main (){
         switch ( hitung )
     {
         case 1 :
-        printf( "Masukan Panjang: ");
+        printf( "Masukan Alas Tinggi Segitiga: ");
         scanf( "%d", &panjang ); 
 
-        printf( "Masukan Lebar: ");
+        printf( "Masukan Alas Lebar Segitiga: ");
         scanf( "%d", &lebar ); 
 
         // Ngitung dulu :v
         luas= 1.2*panjang*lebar;
 
         if ( luas >=80 ){ 
-            printf( "Hasil dari 1.2 x Panjang X Lebar adalah = 1.2 x %d x %d = %d\n", panjang, lebar, luas);
-            printf( "Dan ini adalah Segi Tiga Besar");
-            return 0;
+            printf( "\n\n" );
+            printf( "====================================================\n");
+            printf( "Hasil dari 1.2 x Alas Tinggi Segitiga X Alas Lebar Segitiga adalah = 1.2 x %d x %d = %d\n", panjang, lebar, luas);
+            printf( "Segi Tiga Besar\n");
+            printf( "====================================================\n");
+            break;
         }
         else if ( luas<=80) {
-            printf( "Ini Segi Tiga kecil:(");
-            return 0;
+            printf( "\n\n" );
+            printf( "====================================================\n");
+            printf( "Hasil dari 1.2 x Alas Tinggi Segitiga X Alas Lebar Segitiga adalah = 1.2 x %d x %d = %d\n", panjang, lebar, luas);
+            printf( "Ini Segi Tiga kecil:(\n");
+            printf( "====================================================\n");
+            break;
         }
 
         case 2 :
@@ -51,17 +60,25 @@ int main (){
         luas= panjang*lebar;
 
         if ( luas >=150 ){ 
+            printf( "\n\n" );
+            printf( "====================================================\n");
             printf( "Hasil dari Panjang X Lebar adalah = %d x %d = %d\n", panjang, lebar, luas);
-            printf( "Dan ini adalah persegi Besar");
-            return 0;
+            printf( "Dan ini adalah persegi Besar\n");
+            printf( "====================================================\n");
+            break;
         }
         else if ( luas<=150) {
-            printf( "Ini persegi kecil:(");
-            return 0;
+            printf( "\n\n" );
+            printf( "====================================================\n");
+            printf( "Hasil dari Panjang X Lebar adalah = %d x %d = %d\n", panjang, lebar, luas);
+            printf( "Ini persegi kecil:(\n");
+            printf( "====================================================\n");
+            break;
         }
 
         default :
-        printf ( "Menu yang lu masukin ngga ada -_-");
+        printf( "Menu Tidak Valid" );
+    //   MessageBox(NULL, "Menu Yang lu masukin ngga ada cok", "Note", MB_ABORTRETRYIGNORE);
     }
   }
 }
